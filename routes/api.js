3d7bea0c733af53cb4066892c246d8f2aca2147b
Kris Baily <kristopherbaily@gmail.com>
2021-11-01 20:09:42 -0700

    Added get route

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,18 @@
 const router = require("express").Router();
 const db = require("../models");
 
-router.get('/api/workouts')
+router.get('/api/workouts', (req, res) => {
+    db.Workout.find({})
+    .sort({ date: -1 })
+    .then(dbWorkout => {
+        res.json(dbWorkout)
+    })
+    .catch(err => {
+        res.status(400).json(err)
+    })
+})
 router.get('/api/workouts/range')
 router.post('/api/workouts')
-router.purge('/api/workouts')
+router.put('/api/workouts')
 
 module.exports = router;
