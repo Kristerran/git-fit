4c30898978f873d1a209c3a7a473f0b288adab21
Kris Baily <kristopherbaily@gmail.com>
2021-11-01 19:57:20 -0700

    Added which api routes will be needed(To Do: Flesh out get, put and post routes)

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,9 @@
 const router = require("express").Router();
+const db = require("../models");
+
+router.get('/api/workouts')
+router.get('/api/workouts/range')
+router.post('/api/workouts')
+router.purge('/api/workouts')
 
 module.exports = router;
